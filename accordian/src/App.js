1d6207937ba61b1d78ac78f174981e69b4c1ba0e
Sandeep Solanki <manoritesandeep@gmail.com>
2024-03-17 09:53:57 -0500

    steps v2 initial commit

--- accordian/src/App.js
+++ accordian/src/App.js
@@ -1,4 +1,5 @@
 import "./styles.css";
+import { useState } from "react";
 
 const faqs = [
   {
@@ -21,11 +22,33 @@ const faqs = [
 export default function App() {
   return (
     <div>
-      <Accordion />
+      <Accordion data={faqs}/>
     </div>
   );
 }
 
-function Accordion() {
-  return <div>TODO</div>;
+function Accordion({ data }) {
+  return (
+    <div>
+      {data.map((el, i) => (
+        <AccordionItem title={el.title} text={el.text} num={i} key={i} />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen(!isOpen)
+  }
+
+  return <div className={`item ${isOpen ? "open":""}`} onClick={ handleToggle }>
+    <p className='number'>{num < 9 ? `0${num+1}`: num+1}</p>
+    <p className='title'>{title}</p>
+    <p className='icon'>{isOpen ? "-" : "+"}</p>
+
+    {isOpen && <div className="content-box"> {text} </div>}
+  </div>;
 }
