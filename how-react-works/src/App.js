ff833c2e8bd51032426b798d224f4c24815a1671
Sandeep Solanki <manoritesandeep@gmail.com>
2024-04-07 20:59:10 -0500

    final push for how react works

--- how-react-works/src/App.js
+++ how-react-works/src/App.js
@@ -67,7 +67,27 @@ function TabContent({ item }) {
   const [likes, setLikes] = useState(0);
 
   function handleInc() {
-    setLikes(likes + 1);
+    setLikes((likes) => likes + 1);
+  }
+
+  function handleTripleInc() {
+    // setLikes(likes+1)
+    // setLikes(likes+1)
+    // setLikes(likes+1)
+    // above wont work as expected since asynchronous happens in react
+
+    setLikes((likes) => likes + 1)
+    setLikes((likes) => likes + 1)
+    setLikes((likes) => likes + 1)
+  }
+
+  function handleUndo() {
+    setShowDetails(true)
+    setLikes(0)
+  }
+
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000)
   }
 
   return (
@@ -83,13 +103,13 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
