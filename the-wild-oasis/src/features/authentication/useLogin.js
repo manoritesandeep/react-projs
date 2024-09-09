a20f2b5235b39d1274fc7bf9f29812b3bb7122cd
manoritesandeep <manoritesandeep@gmail.com>
2024-09-08 19:02:40 -0700

    added user logout option and fixed bug in useLogin to setQueryData

--- the-wild-oasis/src/features/authentication/useLogin.js
+++ the-wild-oasis/src/features/authentication/useLogin.js
@@ -1,16 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      console.log(user);
-      navigate("/dashboard");
+      queryClient.setQueryData(["user"], user.user);
+      // console.log(user);
+      navigate("/dashboard", { replace: true });
     },
     onError: (err) => {
       console.log("ERROR", err);
