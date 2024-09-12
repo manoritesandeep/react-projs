8e3d54eb3e9044e60c0a6cd0a136f0eaa1d0a9f7
manoritesandeep <manoritesandeep@gmail.com>
2024-09-12 07:36:05 -0700

    added error boundary

--- the-wild-oasis/src/features/bookings/BookingTable.jsx
+++ the-wild-oasis/src/features/bookings/BookingTable.jsx
@@ -12,7 +12,6 @@ function BookingTable() {
   const { bookings, isLoading, count } = useBookings();
 
   if (isLoading) return <Spinner />;
-
   if (!bookings.length) return <Empty resourceName="booking" />;
 
   return (
