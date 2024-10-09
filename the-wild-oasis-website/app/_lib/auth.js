c7ea456dbf3e0139eeba55f3813f9d714315def8
manoritesandeep <manoritesandeep@gmail.com>
2024-10-08 21:29:27 -0700

    added auth and matcher and middleware

--- the-wild-oasis-website/app/_lib/auth.js
+++ the-wild-oasis-website/app/_lib/auth.js
@@ -8,6 +8,13 @@ const authConfig = {
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
+  callback: {
+    authorized({ auth, request }) {
+      // USE !! to convert any value to a boolean
+      return !!auth?.user;
+      // means if auth.user exists return true else return false
+    },
+  },
 };
 
 export const {
