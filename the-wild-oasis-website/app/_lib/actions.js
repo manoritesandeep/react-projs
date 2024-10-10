e5db791df3e454bd5a36f395ee818cf2494d9aa1
manoritesandeep <manoritesandeep@gmail.com>
2024-10-09 23:10:54 -0700

    Added update profile server component into actions.js file along with Manual cache revalidation using revalidatePath

--- the-wild-oasis-website/app/_lib/actions.js
+++ the-wild-oasis-website/app/_lib/actions.js
@@ -1,6 +1,37 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { revalidatePath } from "next/cache";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
+
+export async function updateGuest(formData) {
+  // console.log(formData);
+
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  // Check if user gives a valid nationalID number... use Regex
+  if (!/^[a-zA-Z0-9]{6,15}$/.test(nationalID))
+    throw new Error("Please provide a valid national ID");
+
+  const updateData = { nationality, countryFlag, nationalID };
+
+  // console.log(updateData);
+
+  // Update data into SupaBase db
+  const { data, error } = await supabase
+    .from("guests")
+    .update(updateData)
+    .eq("id", session.user.guestId);
+
+  if (error) throw new Error("Guest could not be updated");
+
+  // Immediately update data on the page
+  revalidatePath("/account/profile");
+}
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
