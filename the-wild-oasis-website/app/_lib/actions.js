df7b6fcdbc23d8dffdebd6030325bd17c82e0a71
manoritesandeep <manoritesandeep@gmail.com>
2024-10-11 22:59:37 -0700

    Added edit capabilities and updating... added new submit button

--- the-wild-oasis-website/app/_lib/actions.js
+++ the-wild-oasis-website/app/_lib/actions.js
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 import { getBookings } from "./data-service";
+import { redirect } from "next/navigation";
 
 export async function updateGuest(formData) {
   // console.log(formData);
@@ -53,6 +54,45 @@ export async function deleteReservation(bookingId) {
   if (error) throw new Error("Booking could not be deleted");
 
   revalidatePath("/account/reservations");
+}
+
+export async function updateBooking(formData) {
+  const bookingId = Number(formData.get("bookingId"));
+  console.log(formData);
+  // 1) Authentication
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in to perform the action");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingsIds = guestBookings.map((booking) => booking.id);
+
+  // Update own booking only
+  if (!guestBookingsIds.includes(bookingId))
+    throw new Error("You are not permitted to update this booking");
+
+  // Building up data
+  const updateData = {
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+  };
+
+  // STEP 4: Mutation
+  const { error } = await supabase
+    .from("bookings")
+    .update(updateData)
+    .eq("id", bookingId)
+    .select()
+    .single();
+
+  // STEP 5: Error handling
+  if (error) throw new Error("Booking could not be updated");
+
+  // STEP 6: Revalidation
+  revalidatePath(`/account/reservations/edit/${bookingId}`);
+  revalidatePath("/account/reservations");
+
+  // STEP 7: Redirecting
+  redirect("/account/reservations");
 }
 
 export async function signInAction() {
